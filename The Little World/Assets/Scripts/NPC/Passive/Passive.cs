d04f0e673efdcae08ab3e5d26d3192f19c67aa55
CrocksNSocks <60799481+CrocksNSocks@users.noreply.github.com>
2020-02-18 10:29:29 -0600

    added-lil-frien-made-him-move

---
+++ The Little World/Assets/Scripts/NPC/Passive/Passive.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passive : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform locations;
+    public float minX;
+    public float MaxX;
+    public float minY;
+    public float maxY;
+
+
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+
+        locations.position = new Vector2(Random.Range(minX, MaxX), Random.Range(minX, MaxX));
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, locations.position, speed*Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, locations.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                locations.position = new Vector2(Random.Range(minX, MaxX), Random.Range(minX, MaxX));
+                waitTime = startWaitTime;
+            }
+
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
