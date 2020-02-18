aaff48f47c14af0fc94b93fbf0f926a766bf7345
Enejii <29257863+enejii@users.noreply.github.com>
2020-02-18 08:42:37 -0600

    ItemScript

---
+++ The Little World/Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item {
+    public int id;
+    public string name;
+    public string description;
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+    public Item(int id, string name, string description, Dictionary<string, int> stats)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        //this.icon = Resource.load<Sprite>();
+        this.stats = stats;
+    }
+
+    public Item(Item item)
+    {
+        this.id = item.id;
+        this.name = item.name;
+        this.description = item.description;
+        //this.icon = Resource.load<Sprite>();
+        this.stats = item.stats;
+    }
+}
