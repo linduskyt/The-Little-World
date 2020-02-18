21dcef9739d85e8561f4185a9bbb3308079b78f2
CrocksNSocks <60799481+CrocksNSocks@users.noreply.github.com>
2020-02-18 09:15:46 -0600

    added Code

---
+++ The Little World/Assets/Scripts/MenuUIScripts/MainMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+
+    public void LoadPlayMenu()
+    {
+        SceneManager.LoadScene("PlayMenu");
+    }
+
+    public void LoadSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void LoadMainSave()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+}
