54dba366a0a347b0dff8ada6f14dc23db4dc722d
Enejii <29257863+enejii@users.noreply.github.com>
2020-02-18 09:18:10 -0600

    ?

--- The Little World/Assets/Scripts/ItemDatabase.cs
+++ The Little World/Assets/Scripts/ItemDatabase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemScript : MonoBehaviour
+public class ItemDatabase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
