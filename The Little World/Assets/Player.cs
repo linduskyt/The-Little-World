5e41565e07a5cfba54fa5991bc0b83227d184e86
Enejii <29257863+enejii@users.noreply.github.com>
2020-02-24 18:49:26 -0600

    savingAndLoading
    Player can now save and load inventory

--- The Little World/Assets/Player.cs
+++ The Little World/Assets/Player.cs
@@ -16,6 +16,14 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            inventory.Save();
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            inventory.Load();
+    }
+
     private void OnApplicationQuit()
     {
         inventory.Container.Clear();
