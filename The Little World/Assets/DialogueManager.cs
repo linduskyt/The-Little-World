01370b769be014e38bee9c79abc289bb10a29795
Isaac Schulz <ancientroad056@gmail.com>
2020-03-07 14:08:20 -0600

    tried-to-make-progress

---
+++ The Little World/Assets/DialogueManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue()
+    {
+        Debug.Log("Starting conversation with " + Dialogue.name);
+    }
+}
