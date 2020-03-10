3bf2d4dcfeeaf1eb84f9212268b1c1e5a305bad9
Thomas Lindusky <linduskyt1743@my.uwstout.edu>
2020-03-10 10:14:05 -0500

    one more thing

--- The Little World/Assets/Scripts/PlayerWalkAnimated.cs
+++ The Little World/Assets/Scripts/PlayerWalkAnimated.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerWalkAnimated : MonoBehaviour
 {
-    private const int V = 130;
     public float speed = 0.1F;
     public Animator animator;
-    private Rigidbody2D myBody;
+    public Rigidbody2D myBody;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +21,7 @@ public class PlayerWalkAnimated : MonoBehaviour
         float vertTranslation = Input.GetAxis("Vertical") * speed;
 
         myBody.velocity = new Vector2(horizTranslation, vertTranslation);
-        transform.Translate(horizTranslation / V, vertTranslation / V, 0);
+        transform.Translate(horizTranslation, vertTranslation, 0);
 
         animator.SetFloat("VertSpeed", vertTranslation);
         animator.SetFloat("HorizSpeed", horizTranslation);
--- The Little World/Assets/Scripts/PlayerWalking.cs
+++ The Little World/Assets/Scripts/PlayerWalking.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerWalking : MonoBehaviour
 {
-    private const int V = 130;
     public float speed = 0.1F;
 
     // Start is called before the first frame update
@@ -19,6 +18,6 @@ public class PlayerWalking : MonoBehaviour
         float horizTranslation = Input.GetAxis("Horizontal") * speed;
         float vertTranslation = Input.GetAxis("Vertical") * speed;
 
-        transform.Translate(horizTranslation / V, vertTranslation / V, 0);
+        transform.Translate(horizTranslation, vertTranslation, 0);
     }
 }
