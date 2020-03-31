9076e9bd6956d9e6ec478df2e5fa563245ec1119
CrocksNSocks <60799481+CrocksNSocks@users.noreply.github.com>
2020-03-31 15:07:53 -0500

    UI

--- The Little World/Assets/Scripts/NPC/Active/DialogueManager.cs
+++ The Little World/Assets/Scripts/NPC/Active/DialogueManager.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
 
     private Queue<string> sentences = new Queue<string>();
 
+    public GameObject NPCOptions;
+
+    public Text nameText;
+    public Text dialogueText;
+
     // Start is called before the first frame update
     void Start()
     {
          
     }
+
+    void Update()
+    {
+
+    }
     public void StartDialogue(Dialogue dialogue)
     {
-       Debug.Log("Starting conversation with " + dialogue.name);
+       //Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
 
         sentences.Clear();
 
@@ -35,12 +48,17 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        dialogueText.text = sentence;
 
     }
 
     void EndDialogue()
     {
         Debug.Log("End of conversation");
+    }
+
+    public void nextMenu()
+    {
+
     }
 }
