6fccaf06248f54c8316bfa6c4e03da4f13e3029c
linduskyt <60932644+linduskyt@users.noreply.github.com>
2020-04-28 22:09:21 -0500

    Inventory

--- The Little World/Assets/Scripts/Joystick.cs
+++ The Little World/Assets/Scripts/Joystick.cs
@@ -8,39 +8,56 @@ public class Joystick : MonoBehaviour
     public Animator animator;
     public float speed = 5.0f;
     private bool touchStart = false;
+
+    private float cameraPositionX;
+    private float cameraPositionY;
+
+    private Vector2 pointIn;
     private Vector2 pointA;
     private Vector2 pointB;
 
-    public Transform circle;
-    public Transform outerCircle;
+    private Rect bottomLeft;
 
-    public Rigidbody2D myBody;
-    public Rigidbody2D camera;
+    public Transform joyTransform;
+    public Transform outerJoyTransform;
+
+    public Rigidbody2D playerRigidBody;
+    public Rigidbody2D cameraRigidBody;
+
+    public Rigidbody2D joyRigidBody;
+    public Rigidbody2D outerJoyRigidBody;
 
     // Update is called once per frame
     void Update()
     {
+        cameraPositionX = cameraTransform.position.x;
+        cameraPositionY = cameraTransform.position.y;
+
+        bottomLeft = new Rect(cameraPositionX - 8, cameraPositionY - 5, 4, 4);
+
+        pointA = new Vector2(cameraPositionX - 6, cameraPositionY - 3);
+
         if (Input.GetMouseButtonDown(0))
         {
-            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+            pointIn = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
                 Input.mousePosition.y, Camera.main.transform.position.z));
 
-            circle.transform.position = pointA;
-            outerCircle.transform.position = pointA;
-            circle.GetComponent<SpriteRenderer>().enabled = true;
-            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            //joyTransform.transform.position = pointA;
+            //outerJoyTransform.transform.position = pointA;
         }
         if (Input.GetMouseButton(0))
         {
-            touchStart = true;
-            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
-                Input.mousePosition.y, Camera.main.transform.position.z));
+            if (bottomLeft.Contains(pointIn) || touchStart)
+            {
+                touchStart = true;
+                pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                    Input.mousePosition.y, Camera.main.transform.position.z));
+            }
         }
         else
         {
             touchStart = false;
         }
-
     }
 
     private void FixedUpdate()
@@ -49,39 +66,54 @@ public class Joystick : MonoBehaviour
         {
             Vector2 offset = pointB - pointA;
             Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
-            moveCharacter(offset);
+            moveCharacter(direction);
+
+            joyTransform.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
+            outerJoyTransform.transform.position = pointA;
         }
         else
         {
+            playerRigidBody.velocity = new Vector2(0, 0);
             transform.Translate(0, 0, 0);
+            cameraRigidBody.velocity = new Vector2(0, 0);
             cameraTransform.Translate(0, 0, 0);
 
-            circle.GetComponent<SpriteRenderer>().enabled = false;
-            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+            joyTransform.transform.position = pointA;
+            outerJoyTransform.transform.position = pointA;
 
             animator.SetFloat("VertSpeed", 0);
             animator.SetFloat("HorizSpeed", 0);
             animator.SetBool("HorizGreaterThan", false);
         }
-
     }
+
     void moveCharacter(Vector2 direction)
     {
-        float horizTranslation = direction.x * speed * Time.deltaTime;
-        float vertTranslation = direction.y * speed * Time.deltaTime;
+        float horizTranslation = direction.x * speed;
+        float vertTranslation = direction.y * speed;
 
-        myBody.velocity = new Vector2(horizTranslation, vertTranslation);
-        transform.Translate(horizTranslation, vertTranslation, 0);
+        playerRigidBody.velocity = new Vector2(horizTranslation, vertTranslation);
+        //transform.Translate(horizTranslation, vertTranslation, 0);
 
         animator.SetFloat("VertSpeed", vertTranslation);
         animator.SetFloat("HorizSpeed", horizTranslation);
         animator.SetBool("HorizGreaterThan", Mathf.Abs(horizTranslation) > 
             Mathf.Abs(vertTranslation));
 
-        camera.velocity = new Vector2(horizTranslation, vertTranslation);
-        cameraTransform.Translate(horizTranslation, vertTranslation, 0);
+        cameraRigidBody.velocity = new Vector2(horizTranslation, vertTranslation);
+        //cameraTransform.Translate(horizTranslation / 4, vertTranslation / 4, 0);
 
-        circle.Translate(horizTranslation, vertTranslation, 0);
-        outerCircle.Translate(horizTranslation, vertTranslation, 0);
+        //joyRigidBody.velocity = new Vector2(horizTranslation, vertTranslation);
+        //outerJoyRigidBody.velocity = new Vector2(horizTranslation, vertTranslation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
     }
 }
